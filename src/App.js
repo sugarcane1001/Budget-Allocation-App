483afeebaa467fe1dd03340514b76b9c34d9a66d
Mohammed Zian Hassan <139604218+sugarcane1001@users.noreply.github.com>
2023-11-04 16:55:19 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import ExpenseItem from './components/ExpenseItem';
+import AllocationForm from './components/AllocationForm';
+
+
+
+
+import { AppProvider } from './context/AppContext';
+const App = () => {
+    return (
+        <AppProvider>
+            <div className='container'>
+                <h1 className='mt-3'>Budget Allocation</h1>
+                    <div className='row mt-3'>
+                        {
+                            // Budget component
+                            <div className='col-sm'>
+                                <Budget />
+                            </div>
+                        }        
+
+                        {
+                            //Remaining component
+                            <div className='col-sm'>
+                                <Remaining />
+                            </div>
+                        }        
+
+                        {
+                            //ExpenseTotal component
+                            <div className='col-sm'>
+                                <ExpenseTotal />
+                            </div>
+                        }        
+                       
+                        {
+                            <div className='col-sm'>
+                                <ExpenseList />
+                            </div>                        
+                        }         
+
+                        {
+                            <div className='col-sm'>
+                                <ExpenseItem />
+                            </div>                         
+                        }        
+
+                        {
+                            <div className='col-sm'>
+                                <AllocationForm />
+                            </div>                         
+                        }        
+
+                </div>
+            </div>
+        </AppProvider>
+    );
+};
+export default App;
